5c4f7780a1a511825162937dbea91a9cc06b3477
asbdu160cb22s613397 <145563919+asbdu160cb22s613397@users.noreply.github.com>
2023-09-21 10:23:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+def factorial(n)
+if(n==1):
+  return n
+else:
+  return n*factorial(n-1)
+  num=int(input("Enter a number":))
+  if(num<0):
+    print("invalid")
+  else:
+    print("the factorial is",factorial(num))
+         
